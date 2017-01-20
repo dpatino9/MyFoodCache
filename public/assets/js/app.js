30f6d42463d32d920706b6d55c501c1826257511
Adriana Castro <castrocadriana@gmail.com>
2017-01-19 22:41:16 -0500

    handlebars

---
+++ public/assets/js/app.js
@@ -0,0 +1,22 @@
+
+$( document ).ready(function() {
+
+//fadein q
+	$('#1, #2, #3, #4').hide();
+		$('#1').fadeIn(1500, function(){ 
+		$('#2').fadeIn(1500, function(){
+		$('#3').fadeIn(1500, function(){
+		$('#4').fadeIn(1500)
+		})})
+
+
+	});
+
+//mainSubmit
+
+$( "#mainSubmit" ).click(function() {
+	$('.modal').modal();
+	$('#main-content').empty();
+});
+
+});
