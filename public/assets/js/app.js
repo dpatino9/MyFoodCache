21025ec191799d42c6e850ceb232a3e5e5abe346
Eddy Leon <xltunited@gmail.com>
2017-01-23 13:35:52 -0500

    api call connected from pantry.handlebars to '/api' post route

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,35 +2,73 @@
 $( document ).ready(function() {
 
 //fadein q
-	$('#1, #2, #3, #4').hide();
-		$('#1').fadeIn(1500, function(){ 
-		$('#2').fadeIn(1500, function(){
-		$('#3').fadeIn(1500, function(){
-		$('#4').fadeIn(1500)
+	$("#1, #2, #3, #4").hide();
+		$("#1").fadeIn(1500, function(){ 
+		$("#2").fadeIn(1500, function(){
+		$("#3").fadeIn(1500, function(){
+		$("#4").fadeIn(1500)
 		})})
 
 
 	});
 
-//mainSubmit
+	//mainSubmit
 
-$( "#mainSubmit" ).click(function() {
-	$('.modal').modal();
-	$('#main-content').empty();
-});
+	$( "#mainSubmit" ).click(function() {
+		$(".modal").modal();
+		$("#main-content").empty();
+	});
 
-//instrucions 
-$( "#modalIntructions" ).click(function() {
-	$('.modal').modal();
-});
+	//instrucions 
+	$( "#modalIntructions" ).click(function() {
+		$('.modal').modal();
+	});
 
-// favorite recipes
-$(".button-collapse").sideNav();
+	// favorite recipes
+	$(".button-collapse").sideNav();
 
 
-$( ".button-collapse" ).click(function() {
-	$('.brand-logo').hide();
-});
+	$( ".button-collapse" ).click(function() {
+		$(".brand-logo").hide();
+	});
+
+	$("#pantry-submit").on("click", function(){
+
+		var counter = 0;
+
+		var apiUrl = "";
+
+		$("input").each(function(){
+
+			counter++;
+
+			if( $(this).val() != ""){
+
+				apiUrl = apiUrl + $(this).val() + ",";
+
+				
+				console.log(counter);
+
+				console.log(apiUrl);
+
+			}
+
+			if(counter == 12){
+
+				$.post("http://localhost:4000/api",
+			    {
+			        searchQuery: apiUrl
+			    }, function(data, status){
+
+			        console.log(data);
+
+			    });
+
+			}
+
+		});
+
+	})
 
 });
 
--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -12,7 +12,7 @@ var UserSchema = new Schema({
     
   },
 
-  recepies: {
+  recipes: {
     type: Array,
     default: []    
   }
